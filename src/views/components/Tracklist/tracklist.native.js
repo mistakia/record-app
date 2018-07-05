601645dae7276d303920977a78924650b64f94cf
Kia Rahimian <kia@tintmail.com>
2018-07-05 08:17:05 -0400

    feat: add icons

--- src/views/components/Tracklist/tracklist.native.js
+++ src/views/components/Tracklist/tracklist.native.js
@@ -1,25 +1,61 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import { getCurrentTracklist, getTracksForCurrentTracklist } from '@core/tracklists'
+import { getPlayerIsPlaying, getPlayerTrackId, playerActions } from '@core/player'
+import { audio } from '@core/audio'
 import LoadingIndicator from '@components/LoadingIndicator'
 import Track from '@components/Track'
 
-export default function Tracklist ({ tracks, displayLoadingIndicator }) {
-  const trackItems = tracks.map((track, index) => {
-    return (
-      <View key={index}>
-        <Track track={track} />
-      </View>
-    )
+class Tracklist extends React.Component {
+  render () {
+    const {
+      isPlaying,
+      pause,
+      play,
+      selectedTrackId,
+      selectTrack,
+      tracklistId,
+      tracks
+    } = this.props
+
+    const trackItems = tracks.map((track, index) => {
+      const isSelected = track.id === selectedTrackId
+      return (
+        <View key={index}>
+          <Track
+            track={track}
+            isPlaying={isSelected && isPlaying}
+            isSelected={isSelected}
+            pause={pause}
+            play={isSelected ? play : selectTrack.bind(null, track.id, tracklistId)}
+          />
+        </View>
+      )
+    })
+  }
+}
+
+const mapStateToProps = createSelector(
+  getPlayerIsPlaying,
+  getPlayerTrackId,
+  getCurrentTracklist,
+  getTracksForCurrentTracklist,
+  (isPlaying, playerTrackId, tracklist, tracks) => ({
+    displayLoadingIndicator: tracklist.isPending,
+    isPlaying,
+    pause: audio.pause,
+    play: audio.play,
+    selectedTrackId: playerTrackId,
+    tracklistId: tracklist.id,
+    tracks
   })
 
-  return (
-    <View>
-      {trackItems}
-
-      <View>
-        {(displayLoadingIndicator) ? <LoadingIndicator /> : null}
-      </View>
-    </View>
-  )
+const mapDispatchToProps = {
+  selectTrack: playerActions.playSelectedTrack
 }
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Tracklist)
