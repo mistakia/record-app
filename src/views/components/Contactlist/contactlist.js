c95cd55b1441bbb308548c389552f955ff115b58
Kia Rahimian <kia@tintmail.com>
2018-07-07 13:45:35 -0400

    refractor: separate component & container logic

--- src/views/components/Contactlist/contactlist.js
+++ src/views/components/Contactlist/contactlist.js
@@ -1,45 +1,24 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { createSelector } from 'reselect'
 
-import {
-  getCurrentContactlist,
-  getContactsForCurrentContactlist
-} from '@core/contactlists'
 import LoadingIndicator from '@components/LoadingIndicator'
 import Contact from '@components/Contact'
 
-class Contactlist extends React.Component {
-  render () {
-    const { contacts } = this.props
-
-    const contactItems = contacts.map((contact, index) => {
-      return (
-        <div key={index}>
-          <Contact contact={contact} />
-        </div>
-      )
-    })
-
+export default function Contactlist ({ contacts, displayLoadingIndicator }) {
+  const contactItems = contacts.map((contact, index) => {
     return (
-      <div>
-        {contactItems}
-
-        <div>
-          { this.props.displayLoadingIndicator ? <LoadingIndicator /> : null}
-        </div>
+      <div key={index}>
+        <Contact contact={contact} />
       </div>
     )
-  }
-}
-
-const mapStateToProps = createSelector(
-  getCurrentContactlist,
-  getContactsForCurrentContactlist,
-  (contactlist, contacts) => ({
-    displayLoadingIndicator: contactlist.isPending,
-    contacts
   })
-)
 
-export default connect(mapStateToProps, null)(Contactlist)
+  return (
+    <div>
+      {contactItems}
+
+      <div>
+        { displayLoadingIndicator ? <LoadingIndicator /> : null}
+      </div>
+    </div>
+  )
+}
--- src/views/components/Contactlist/index.js
+++ src/views/components/Contactlist/index.js
@@ -1,1 +1,20 @@
-export { default } from './contactlist'
+import { connect } from 'react-redux'
+import { createSelector } from 'reselect'
+
+import {
+  getCurrentContactlist,
+  getContactsForCurrentContactlist
+} from '@core/contactlists'
+
+import Contactlist from './contactlist'
+
+const mapStateToProps = createSelector(
+  getCurrentContactlist,
+  getContactsForCurrentContactlist,
+  (contactlist, contacts) => ({
+    displayLoadingIndicator: contactlist.isPending,
+    contacts
+  })
+)
+
+export default connect(mapStateToProps, null)(Contactlist)
