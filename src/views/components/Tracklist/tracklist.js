39354e3fc4fa88aac0c28ea1d280cb2922e37306
mistakia <1823355+mistakia@users.noreply.github.com>
2018-07-07 13:50:41 -0400

    refractor: split container & component logic

--- src/views/components/Tracklist/tracklist.js
+++ src/views/components/Tracklist/tracklist.js
@@ -1,61 +1,39 @@
 import React from 'react'
 
-import { getCurrentTracklist, getTracksForCurrentTracklist } from '@core/tracklists'
-import { getPlayerIsPlaying, getPlayerTrackId, playerActions } from '@core/player'
-import { audio } from '@core/audio'
 import LoadingIndicator from '@components/LoadingIndicator'
 import Track from '@components/Track'
 
-class Tracklist extends React.Component {
-  render () {
-    const {
-      isPlaying,
-      pause,
-      play,
-      selectedTrackId,
-      selectTrack,
-      tracklistId,
-      tracks
-    } = this.props
-
-    const trackItems = tracks.map((track, index) => {
-      const isSelected = track.id === selectedTrackId
-      return (
-        <div key={index}>
-          <Track
-            track={track}
-            isPlaying={isSelected && isPlaying}
-            isSelected={isSelected}
-            pause={pause}
-            play={isSelected ? play : selectTrack.bind(null, track.id, tracklistId)}
-          />
-        </div>
-      )
-    })
-
-  }
-}
-
-const mapStateToProps = createSelector(
-  getPlayerIsPlaying,
-  getPlayerTrackId,
-  getCurrentTracklist,
-  getTracksForCurrentTracklist,
-  (isPlaying, playerTrackId, tracklist, tracks) => ({
-    displayLoadingIndicator: tracklist.isPending,
-    isPlaying,
-    pause: audio.pause,
-    play: audio.play,
-    selectedTrackId: playerTrackId,
-    tracklistId: tracklist.id,
-    tracks
+export default function Tracklist ({
+  isPlaying,
+  pause,
+  play,
+  selectedTrackId,
+  selectTrack,
+  tracklistId,
+  tracks
+}) {
+  const trackItems = tracks.map((track, index) => {
+    const isSelected = track.id === selectedTrackId
+    return (
+      <div key={index}>
+        <Track
+          track={track}
+          isPlaying={isSelected && isPlaying}
+          isSelected={isSelected}
+          pause={pause}
+          play={isSelected ? play : selectTrack.bind(null, track.id, tracklistId)}
+        />
+      </div>
+    )
   })
 
-const mapDispatchToProps = {
-  selectTrack: playerActions.playSelectedTrack
-}
+  return (
+    <div>
+      {trackItems}
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Tracklist)
+      <div>
+        {(this.props.displayLoadingIndicator) ? <LoadingIndicator /> : null}
+      </div>
+    </div>
+  )
+}
