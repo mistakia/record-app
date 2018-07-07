e0519b7ebc21567e5dac97a3729610096f25b22d
Kia Rahimian <kia@tintmail.com>
2018-07-07 13:50:41 -0400

    refractor: split container & component logic

--- src/views/components/Tracklist/index.js
+++ src/views/components/Tracklist/index.js
@@ -2,16 +2,32 @@ import { connect } from 'react-redux'
 import { createSelector } from 'reselect'
 
 import { getCurrentTracklist, getTracksForCurrentTracklist } from '@core/tracklists'
+import { getPlayerIsPlaying, getPlayerTrackId, playerActions } from '@core/player'
+import { audio } from '@core/audio'
 
 import Tracklist from './tracklist'
 
 const mapStateToProps = createSelector(
+  getPlayerIsPlaying,
+  getPlayerTrackId,
   getCurrentTracklist,
   getTracksForCurrentTracklist,
-  (tracklist, tracks) => ({
+  (isPlaying, playerTrackId, tracklist, tracks) => ({
     displayLoadingIndicator: tracklist.isPending,
+    isPlaying,
+    pause: audio.pause,
+    play: audio.play,
+    selectedTrackId: playerTrackId,
+    tracklistId: tracklist.id,
     tracks
   })
 )
 
-export default connect(mapStateToProps, null)(Tracklist)
+const mapDispatchToProps = {
+  selectTrack: playerActions.playSelectedTrack
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Tracklist)
