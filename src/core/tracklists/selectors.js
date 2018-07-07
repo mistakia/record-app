b7bff30645a8b20aa21617800a1c288efab83e6e
mistakia <1823355+mistakia@users.noreply.github.com>
2018-07-07 10:25:47 -0400

    style: remove semi-colons

--- src/core/tracklists/selectors.js
+++ src/core/tracklists/selectors.js
@@ -16,20 +16,20 @@ export function getCurrentTracklist (state) {
 }
 
 export function getTracklistCursor(selectedTrackId, trackIds) {
-  let index = trackIds.indexOf(selectedTrackId);
-  let nextTrackId = null;
-  let previousTrackId = null;
+  let index = trackIds.indexOf(selectedTrackId)
+  let nextTrackId = null
+  let previousTrackId = null
 
   if (index !== -1) {
-    if (index < trackIds.size - 1) nextTrackId = trackIds.get(index + 1);
-    if (index > 0) previousTrackId = trackIds.get(index - 1);
+    if (index < trackIds.size - 1) nextTrackId = trackIds.get(index + 1)
+    if (index > 0) previousTrackId = trackIds.get(index - 1)
   }
 
   return {
     nextTrackId,
     previousTrackId,
     selectedTrackId
-  };
+  }
 }
 
 //= ====================================
