b7a5716bbccd37e373bfb54eb8a4b323d588b413
Kia Rahimian <kia@tintmail.com>
2018-07-23 16:54:18 -0400

    refactor: separate TrackPage component

--- src/views/pages/tracks/tracks.js
+++ src/views/pages/tracks/tracks.js
@@ -1,50 +1,23 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
-import { tracklistActions } from '@core/tracklists'
 import Tracklist from '@components/Tracklist'
 import PageLayout from '@layouts/page'
 
-export class TracksPage extends React.Component {
-  componentWillMount () {
-    this._load()
-  }
+export default function () {
+  const { logId } = this.props.match.params
+  const head = (
+    <div>
+      <h1>Tracks</h1>
+      { logId === 'me' && <Link className='button' to='/tracks/new'>Add Track</Link> }
+    </div>
+  )
 
-  componentDidUpdate() {
-    this._load()
-  }
+  const body = (
+    <Tracklist />
+  )
 
-  _load () {
-    // '/me' or proper orbitdb address
-    const { logId } = this.props.match.params
-    this.props.loadTracks(logId)
-  }
-
-  render () {
-    const { logId } = this.props.match.params
-    const head = (
-      <div>
-        <h1>Tracks</h1>
-        { logId === 'me' && <Link className='button' to='/tracks/new'>Add Track</Link> }
-      </div>
-    )
-
-    const body = (
-      <Tracklist />
-    )
-
-    return (
-      <PageLayout head={head} body={body} />
-    )
-  }
+  return (
+    <PageLayout head={head} body={body} />
+  )
 }
-
-const mapDispatchToProps = {
-  loadTracks: tracklistActions.loadTracks
-}
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(TracksPage)
--- src/views/pages/tracks/tracks.native.js
+++ src/views/pages/tracks/tracks.native.js
@@ -1,54 +1,27 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { View, Text } from 'react-native'
 import { Link } from 'react-router-native'
 
-import { tracklistActions } from '@core/tracklists'
 import Tracklist from '@components/Tracklist'
 import PageLayout from '@layouts/page'
 import headStyles from '@styles/head'
 
-export class TracksPage extends React.Component {
-  componentWillMount () {
-    this._load()
-  }
+export default function () {
+  const { logId } = this.props.match.params
+  const head = (
+    <View style={headStyles.content}>
+      <Text>Tracks</Text>
+      {logId === 'me' && <Link style={headStyles.button} to='/tracks/new'>
+        <Text>Add Track</Text>
+      </Link>}
+    </View>
+  )
 
-  componentDidUpdate () {
-    this._load()
-  }
+  const body = (
+    <Tracklist />
+  )
 
-  _load () {
-    // '/me' or proper orbitdb adadress
-    const { logId } = this.props.match.params
-    this.props.loadTracks(`/${logId}`)
-  }
-
-  render () {
-    const { logId } = this.props.match.params
-    const head = (
-      <View style={headStyles.content}>
-        <Text>Tracks</Text>
-        {logId === 'me' && <Link style={headStyles.button} to='/tracks/new'>
-          <Text>Add Track</Text>
-        </Link>}
-      </View>
-    )
-
-    const body = (
-      <Tracklist />
-    )
-
-    return (
-      <PageLayout head={head} body={body} />
-    )
-  }
+  return (
+    <PageLayout head={head} body={body} />
+  )
 }
-
-const mapDispatchToProps = {
-  loadTracks: tracklistActions.loadTracks
-}
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(TracksPage)
