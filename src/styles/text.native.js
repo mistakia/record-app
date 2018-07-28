603c16f5de5cabc0b930c132a334ffb9904ce507
Kia Rahimian <kia@tintmail.com>
2018-07-28 10:14:18 -0400

    feat: contact component styling

---
+++ src/styles/text.native.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold'
+  },
+  small: {
+    fontSize: 10
+  }
+})
+
+export default styles
