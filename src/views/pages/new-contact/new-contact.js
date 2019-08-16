88e3ddbf4786e670e1d272d563d6d4f5b48c7186
mistakia <1823355+mistakia@users.noreply.github.com>
2019-08-16 15:19:51 -0400

    feat: show contact address on edit/new page

--- src/views/pages/new-contact/new-contact.js
+++ src/views/pages/new-contact/new-contact.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import hashicon from 'hashicon'
 
 import { parseQueryString } from '@core/utils'
 import { contactlistActions } from '@core/contactlists'
 import PageLayout from '@layouts/page'
 import Button from '@components/button'
+import CopyText from '@components/copy-text'
 
 import './new-contact.styl'
 
@@ -18,9 +20,13 @@ export class NewContactPage extends React.Component {
   handleSubmit (event) {
     const data = {
       alias: event.target.alias.value,
-      address: event.target.address.value
+      address: this.props.match.params.logId
     }
     const { app } = this.props
+
+    if (!data.address && event.target.address) {
+      data.address = event.target.address.value
+    }
 
     if (data.address) {
       this.props.addContact(app.address, data)
@@ -33,16 +39,25 @@ export class NewContactPage extends React.Component {
     const { logId } = this.props.match.params
     const { alias, haveContact } = parseQueryString(this.props.location.search)
 
+    const addressIcon = logId ? hashicon(logId, 40) : null
+
     const body = (
       <form id='new-contact' onSubmit={this.handleSubmit}>
         <label>
           Alias
           <input type='text' name='alias' defaultValue={alias} placeholder='Contact Nickname' />
         </label>
-        <label>
-          Address
-          <input type='text' name='address' defaultValue={logId} placeholder='/orbitdb/Qm.../record' disabled={!!haveContact} required />
-        </label>
+        { logId
+          ? <CopyText text={logId}>
+            <label>Library Address</label>
+            <img src={addressIcon && addressIcon.toDataURL()} />
+            <small>{logId}</small>
+          </CopyText>
+          : <label>
+            Address
+            <input type='text' name='address' defaultValue={logId} placeholder='/orbitdb/Qm.../record' disabled={!!haveContact} required />
+          </label>
+        }
         <Button type='submit' isLoading={this.props.isUpdating}>{haveContact ? 'Save' : 'Connect'}</Button>
       </form>
     )
